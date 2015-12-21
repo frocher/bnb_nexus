0b02f35b917fb3449fb18d262f81cba9d71a2364
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2015-12-21 19:11:51 +0100

    wip

--- app/controllers/pages/uptimes_controller.rb
+++ app/controllers/pages/uptimes_controller.rb
@@ -1,14 +1,14 @@
 class Pages::UptimesController < ApplicationController
 
   # Show uptimes for a given page and for a given period
-  # returns either a single value when type is 'point' or a list of value when the type is 'points'
+  # returns either a single value when type is 'median' or a list of value when the type is 'list'
   def show
     @type = params[:type]
     @page = Page.find(params[:page_id])
     @start_date = Date.parse(params[:start]).beginning_of_day
     @end_date   = Date.parse(params[:end]).end_of_day
 
-    if type == 'point'
+    if type == 'median'
     else
       # TODO : we must not have more than X points to display
       # we must so calculate the correct interval between points
