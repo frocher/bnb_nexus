01ef9f34568f4ef7f35cb64b815892c8a62e237a
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2016-02-14 18:40:13 +0100

    puma config update

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,13 @@
-# config/puma.rb
-threads 1, 6
-workers 2
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RAILS_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
