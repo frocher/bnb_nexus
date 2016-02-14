d68fd3d153131cf02b8be56d40c02643fe880410
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2016-02-14 23:12:55 +0100

    changed mailer conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'www.botnbot.com', port: 80 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  config.action_mailer.smtp_settings = { address: "mail", port: 25 }
 end
 
 Sidekiq::Logging.logger.level = Logger::WARN
