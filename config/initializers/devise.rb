9328feaf2a4fced77ad13458b36d4cf5e1c60aac
Frederic Rocher <fredericrocher@macbook-pro-de-frederic.home>
2016-02-14 23:23:49 +0100

    changed jeeves mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.mailer_sender = 'jeeves@thewatchbots.com'
+  config.mailer_sender = 'jeeves@botnbot.com'
   config.secret_key = Figaro.env.devise_secret_key if Rails.env.production?
 end
