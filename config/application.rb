c2f8daddbda2d6cf038560d80602561a25a13158
Fred Rocher <frocher2005@gmail.com>
2016-03-13 15:01:01 +0100

    Change Twb to Bnb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TwbNexus
+module BnbNexus
   class Application < Rails::Application
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
