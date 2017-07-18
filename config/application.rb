f43d2d175b0586fa749014dae2cad980dba89680
Fred Rocher <frocher2005@gmail.com>
2017-07-18 14:06:43 +0200

    Update application configuration

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,18 @@ module BnbNexus
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use sidekiq
+    config.active_job.queue_adapter = :sidekiq
+
+    # Array of probes for pages checks and measures
+    config.probes = JSON.parse(Figaro.env.probes)
+
+    # Omniauth middleware
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
