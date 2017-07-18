fbbe0012cdfea9cf22af8e83cd00297d212dca53
Fred Rocher <frocher2005@gmail.com>
2017-07-18 14:04:33 +0200

    Update Devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,11 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.mailer_sender = 'jeeves.thebot@botnbot.com'
-  config.secret_key = Figaro.env.devise_secret_key if Rails.env.production?
+  config.mailer_sender = ENV.fetch("MAILER_SENDER") { "jeeves.thebot@botnbot.com" }
+  config.secret_key = ENV.fetch("DEVISE_SECRET_KEY") if Rails.env.production?
+
+  # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
+  # middleware b/c rails-api does not include it.
+  # See: http://stackoverflow.com/q/19600905/806956
+  config.navigational_formats = [:json]
 end
