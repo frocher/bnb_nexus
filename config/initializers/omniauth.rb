b074aa3e1299dd84db3c9664040c50ed93fd23c3
Fred Rocher <frocher2005@gmail.com>
2017-07-19 16:49:53 +0200

    Fix omniauth configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,6 @@
-::OmniAuthConfig = Proc.new do
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :github,        ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
   provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET']
-end
-
-OmniAuth.config.on_failure = Proc.new do |env|
-  Users::OmniauthCallbacksController.action(:omniauth_failure).call(env)
 end
