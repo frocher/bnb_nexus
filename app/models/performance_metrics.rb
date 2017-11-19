60aea0e49395e19cafeb6bacb0c1cf65880768bd
Fred Rocher <frocher2005@gmail.com>
2017-11-19 16:07:05 +0100

    Remove unused metric

--- app/models/performance_metrics.rb
+++
@@ -1,8 +0,0 @@
-class PerformanceMetrics < Influxer::Metrics
-  set_series :performance
-  tags :page_id, :target, :probe
-  attributes :response_start, :first_paint, :speed_index, :dom_ready, :page_load_time
-
-  scope :by_page, -> (id) { where(page_id: id) if id.present? }
-  scope :by_target, -> (target) { where(target: target) if target.present? }
-end
