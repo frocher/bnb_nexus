02452ed51550c5282ad3ebc423f74f412c921946
Fred Rocher <frocher2005@gmail.com>
2018-10-07 20:27:04 +0200

    Use letter_opener gem in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,9 +46,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Mailer
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.logger = ActiveSupport::Logger.new(STDOUT)
 
   # Jobs start
