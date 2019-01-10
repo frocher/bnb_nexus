6346a6c9329e707c1c3d9fb4cb529e8f5ab9140e
Fred Rocher <frocher2005@gmail.com>
2019-01-10 15:17:09 +0100

    Add locked attribute to pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181103153016) do
+ActiveRecord::Schema.define(version: 20190110124928) do
 
   create_table "budgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "page_id", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20181103153016) do
     t.integer "uptime_status", default: 0
     t.boolean "push_notify", default: true
     t.integer "device", default: 0
+    t.boolean "locked", default: false
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
